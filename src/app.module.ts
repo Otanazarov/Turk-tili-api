c530dde00ca2d5f013eaea7fcfbfa38f5e3dcab6
Tanjiro017 <otabekotanazarov77@gmail.com>
2025-07-11 11:55:53 +0500

    fixed bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,7 +30,9 @@ import { ListeningAndReadingModule } from './modules/listening-and-reading/liste
     FileModule,
     FollowsModule,
     AuthModule,
+    
     SmsModule,
   ],
 })
 export class AppModule {}
+
